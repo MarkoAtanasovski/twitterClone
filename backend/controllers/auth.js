dedc8db4255588e3439b3f476e454b7e7e41a218
Marko Atanasovski <marko.atanasovski95@gmail.com>
2024-12-31 00:02:14 +0100

    1-project setup

---
+++ backend/controllers/auth.js
@@ -0,0 +1,15 @@
+export const signup = async (req, res) => {
+    res.json({
+        data: 'Your account has been signed up'
+    });
+    }
+export const login = async (req, res) => {
+    res.json({
+        data: 'Your account has been logged in'
+    });
+    }
+export const logout = async (req, res) => {
+    res.json({
+    data: 'Your account has been logged in'
+    });
+    }
