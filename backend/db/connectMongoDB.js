d1fda728e99a668baa0ee9b323d23ee49b75f52c
Marko Atanasovski <marko.atanasovski95@gmail.com>
2025-01-07 16:36:31 +0100

    1-auth controllers added

--- backend/db/connectMongoDB.js
+++ backend/db/connectMongoDB.js
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 
 const connectMongoDB = async()=>{
     try{
+        console.log('Mongo URI:', process.env.MONGO_URI);
         const conn = await mongoose.connect(process.env.MONGO_URI)
         console.log(`Connected to MongoDB: ${conn.connection.host}`);
 
--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,19 @@ import express from 'express';
 import dotenv from 'dotenv'
 import authRoutes from './routes/auth.js';
 import connectMongoDB from './db/connectMongoDB.js';
+import cookieParser from 'cookie-parser';
 
-dotenv.config();
+dotenv.config({ path: 'C:/Users/User/Desktop/TWITTER-CLONE/.env' });
+console.log(process.env);
+
 const app = express();
 const PORT = process.env.PORT || 5000
 
+app.use(express.json()); // to parse req.body
 
+app.use(express.urlencoded({ extended:true})); // to parse form data (urlencoded)
+
+app.use(cookieParser()); // to parse
 
 app.use('/api/auth', authRoutes)
 app.listen(PORT, ()=>{
