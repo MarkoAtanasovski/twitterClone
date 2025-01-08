c0b6d4a88aae3f68961860e347d3cf4ede1cfe6f
Marko Atanasovski <marko.atanasovski95@gmail.com>
2025-01-08 23:56:14 +0100

    1-user routes and controllers added

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,23 @@
 import express from 'express';
 import dotenv from 'dotenv'
-import authRoutes from './routes/auth.js';
-import connectMongoDB from './db/connectMongoDB.js';
 import cookieParser from 'cookie-parser';
+import { v2 as cloudinary } from 'cloudinary';
+
+import authRoutes from './routes/auth.js';
+import userRoutes from './routes/user.js';
+
+import connectMongoDB from './db/connectMongoDB.js';
+
 
 dotenv.config({ path: 'C:/Users/User/Desktop/TWITTER-CLONE/.env' });
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+
+
 console.log(process.env);
 
 const app = express();
@@ -17,6 +30,8 @@ app.use(express.urlencoded({ extended:true})); // to parse form data (urlencoded
 app.use(cookieParser()); // to parse
 
 app.use('/api/auth', authRoutes)
+app.use('/api/users', userRoutes)
+
 app.listen(PORT, ()=>{
     console.log(`Server is running on port: ${PORT}`);
     connectMongoDB();
